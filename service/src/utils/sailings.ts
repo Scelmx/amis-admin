32d1fb1cff12a2a0076aca6947bed050661f919c
王元珑 <1363611525@qq.com>
2024-10-15 10:11:06 +0800

    feat:更新调度算法

--- service/src/utils/sailings.ts
+++ service/src/utils/sailings.ts
@@ -3,5 +3,25 @@ export enum sailings {
     /** 白班 */
     white = '18:00:00',
     /** 夜班 */
-    black = '07:00:00'
+    black = '07:00:00',
+}
+/** 获取白班时间 */
+export const getWhiteTime = () => {
+    // 白班时间 = 白班下班时间-夜班下班时间
+    var [wh,wm,ws] = sailings.white.split(":");
+    var [bh,bm,bs] = sailings.black.split(":");
+    return ((parseInt(wh,10)*3600 + parseInt(wm,10)*60 + parseInt(ws,10))
+        - (parseInt(bh,10)*3600 + parseInt(bm,10)*60 + parseInt(bs,10)))/3600
+}
+export const getBlackTime = () => {
+    var [wh,wm,ws] = sailings.white.split(":");
+    var [bh,bm,bs] = sailings.black.split(":");
+    return (24*3600 - (parseInt(wh,10)*3600 + parseInt(wm,10)*60 + parseInt(ws,10))
+        + (parseInt(bh,10)*3600 + parseInt(bm,10)*60 + parseInt(bs,10)))/3600
+}
+export const getWhiteHour=()=>{
+    return parseInt(sailings.white.split(":")[0], 10)
+}
+export const getBlackHour=()=>{
+    return parseInt(sailings.black.split(":")[0], 10)
 }
